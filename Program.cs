fe7992667c712b5ca941657107d50466289cad27
jancaitis <79664863+jancaitis@users.noreply.github.com>
2021-04-15 13:04:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace projektz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter number of arrays: ");
+            string input = Console.ReadLine();
+            int arraysize = Convert.ToInt32(input);
+            int[] skaitli = new int[arraysize];
+            string sep = "";
+            for (int i = 0; i < arraysize; i++)
+            {
+                Console.Write("which number u select?: ");
+                string userInput = Console.ReadLine();
+                int secondInput = Convert.ToInt32(userInput);
+                skaitli[i] = secondInput;
+
+            }
+            for (int i = 0; i < arraysize; i++)
+                if (skaitli[i] != 0)
+                {
+
+                    Console.Write(skaitli[i]);
+                    sep = ",";
+                    Console.Write(sep);
+                }
+            sep = "";
+            for (int i = 0; i < arraysize; i++)
+                if (skaitli[i] == 0)
+                {
+                    Console.Write(sep);
+                    Console.Write(skaitli[i]);
+                    sep = ",";
+                }
+
+
+
+        }
+    }
+}
